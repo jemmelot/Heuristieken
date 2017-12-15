700523105a1dc61397542d2c78eb4f7928e54998
baszwanenburg <31651649+baszwanenburg@users.noreply.github.com>
2017-12-15 11:27:42 +0100

    Hillclimber en random search aangepast

--- algorithms/Random_search.py
+++ algorithms/Random_search.py
@@ -1,36 +1,41 @@
-import numpy as np
+#!/usr/bin/python3
+# https://github.com/jemmelot/Heuristieken.git
 
-def random_route(main_array, starting_stations, stations):
+import numpy as np
+sys.path.append('../classes/')
+from createnetwerk import createNetwerk
+
+def random_route(trains_amount):
+	"""Calculates routes at random."""
+
 	# the seven calculated routes will be stored in this variable
-	route = [[] for i in range(7)]
+	route = [[] for i in range(trains_amount)]
 	
 	# instantiate numpy array to keep track of how often every station is visited
-	visited_connections = np.zeros((len(stations), len(stations)))
+	visited_connections = np.zeros((22, 22))
 	
 	# create seven random routes
-	for i in range(7):
+	for i in range(trains_amount):
 		total_time = 0
 		
-		starting_station = np.random.choice(starting_stations)
-		if any(starting_station in s for s in stations):
-			starting_station = stations.index(starting_station)
-		
+		rnd = np.random.choice(starting_stations)
+		starting_station = stations.index(rnd)
 		route[i].append(stations[starting_station])
 		
 		def routes(starting_station, total_time):
-			x = np.where(main_array[starting_station, :] > 0)
+			x = np.where(array[starting_station, :] > 0)
 			r = np.random.choice(x[0])
 
-			if (total_time + main_array[starting_station][r]) > 120:
+			if (total_time + array[starting_station][r]) > 120:
 				route[i].insert(0, total_time)
 								
 			else:
 				route[i].append(stations[r])
-				total_time += main_array[starting_station][r]
+				total_time += array[starting_station][r]
 				visited_connections[starting_station][r] += 1
 				visited_connections[r][starting_station] += 1
 				routes(r, total_time)
-				
+
 		routes(starting_station, total_time)
 
-	return visited_connections, route	
+	return visited_connections, route
