ca63e2e4d9cd11d7622b44ff97523a2718933e1c
jemmelot <31655951+jemmelot@users.noreply.github.com>
2017-12-18 21:16:44 +0100

    breadth first correcte csv paths

--- algorithms/Breadth_first.py
+++ algorithms/Breadth_first.py
@@ -88,103 +88,108 @@ returns: best scoring set of routes after all iterations, also the score itself
 	all_time_highest_score = 0
 	all_time_highest_route = [[] for i in range(trains)]
 		
-	with open("holland.csv", 'a', newline='') as f:
-		writer = csv.writer(f)
-		# calculate the best score from a set of random starting stations 100 times
-		try:
-			for i in range(iterations):
-				'''
-				Make a list of random starting stations. Iterate over every option to traverse the tree of a station. 
-				First, determine the best scoring route for	just the first starting station. The second route will have its
-				score combined with that of the first. The third will have its score combined with the first and second, and so on.
-				This way every next best route will be determined based on how it fits in with the already made routes.
-				'''
-				# matrix to track how many times each station has been visited
-				visited_connections = np.zeros((len(stations), len(stations)))
+	# calculate the best score from a set of random starting stations 100 times
+	try:
+		for i in range(iterations):
+			'''
+			Make a list of random starting stations. Iterate over every option to traverse the tree of a station. 
+			First, determine the best scoring route for	just the first starting station. The second route will have its
+			score combined with that of the first. The third will have its score combined with the first and second, and so on.
+			This way every next best route will be determined based on how it fits in with the already made routes.
+			'''
+			# matrix to track how many times each station has been visited
+			visited_connections = np.zeros((len(stations), len(stations)))
+					
+			# make a list of random starting stations		
+			indexes = list(np.random.choice(range(len(stations)), trains, replace=False))
+												
+			# keep track of best scoring routes
+			highest_score = 0.0
+			highest_p = 0
+			highest_route = [[] for l in range(trains)]
+			
+			# make a temporary matrix to copy other arrays into in between loops
+			temp = np.zeros((len(stations), len(stations)))
+					
+			# store routes in one big list
+			route = [[] for l in range(trains)]
+					
+			'''
+			first determine best scoring route from the first set of start-end possibilities,
+			then take that route into consideration at the next route etc.
+			'''
+			for m in range(trains):
+				# check all possibilities between two stations
+				for possibility in reach[indexes[m]]:
+					# clear currect route for every possibility
+					route[m].clear()
+					
+					# variables to track the total travel time of a route
+					total_time = 0
+					time = 0
+					
+					visited_connections = temp.copy()
+					
+					'''
+					For every option from a starting station, cut it to a maximum of 120 (or 180) minutes 
+					'''
+					for station in possibility:						
+						if possibility.index(station) == 0:
+							route[m].append(station)
 						
-				# make a list of random starting stations		
-				indexes = list(np.random.choice(range(len(stations)), trains, replace=False))
-													
-				# keep track of best scoring routes
-				highest_score = 0.0
-				highest_p = 0
-				highest_route = [[] for l in range(trains)]
-				
-				# make a temporary matrix to copy other arrays into in between loops
-				temp = np.zeros((len(stations), len(stations)))
-						
-				# store routes in one big list
-				route = [[] for l in range(trains)]
-						
-				'''
-				first determine best scoring route from the first set of start-end possibilities,
-				then take that route into consideration at the next route etc.
-				'''
-				for m in range(trains):
-					# check all possibilities between two stations
-					for possibility in reach[indexes[m]]:
-						# clear currect route for every possibility
-						route[m].clear()
-						
-						# variables to track the total travel time of a route
-						total_time = 0
-						time = 0
-						
-						visited_connections = temp.copy()
-						
-						'''
-						For every option from a starting station, cut it to a maximum of 120 (or 180) minutes 
-						'''
-						for station in possibility:						
-							if possibility.index(station) == 0:
-								route[m].append(station)
+						else:
+							# determine the travel time between two stations
+							time = int(main_array[stations.index(possibility[possibility.index(station) - 1])][stations.index(station)])
 							
+							if (total_time + time) > 120:
+								break
+							
+							# if allowed by time, add station and its travel time to a route
 							else:
-								# determine the travel time between two stations
-								time = int(main_array[stations.index(possibility[possibility.index(station) - 1])][stations.index(station)])
-								
-								if (total_time + time) > 120:
-									break
-								
-								# if allowed by time, add station and its travel time to a route
-								else:
-									route[m].append(station)
-									visited_connections[stations.index(possibility[possibility.index(station) - 1])][stations.index(station)] += 1
-									visited_connections[stations.index(station)][stations.index(possibility[possibility.index(station) - 1])] += 1
-									total_time += time
-						
-						route[m].insert(0, total_time)
-						
-						'''
-						Calculate the score for the current combination of routes. For this algorithm, the score is calculated
-						using a different score function than the greedy and hill climber algorithms. This is because this algorithm uses a matrix
-						comparison between the main matrix and the matrix containing visited stations/connnections of a set of routes to determine 
-						the percentage of critical stations, whereas the others use modified versions of the list containing all direct connections.
-						Both methods of calculating the score work as intented and produce the same output.
-						'''
-						final_score = float(score_integrated(main_array, visited_connections, stations, route, critical_stations,(m+1)))
-						
-						# if a combination of routes gets a new highest score, set it as the current best scoring combination for following iterations
-						if final_score > highest_score:
-							highest_score = final_score
-							highest_route[m] = route[m].copy()
-							highest_visited = visited_connections.copy()
+								route[m].append(station)
+								visited_connections[stations.index(possibility[possibility.index(station) - 1])][stations.index(station)] += 1
+								visited_connections[stations.index(station)][stations.index(possibility[possibility.index(station) - 1])] += 1
+								total_time += time
+					
+					route[m].insert(0, total_time)
+					
+					'''
+					Calculate the score for the current combination of routes. For this algorithm, the score is calculated
+					using a different score function than the greedy and hill climber algorithms. This is because this algorithm uses a matrix
+					comparison between the main matrix and the matrix containing visited stations/connnections of a set of routes to determine 
+					the percentage of critical stations, whereas the others use modified versions of the list containing all direct connections.
+					Both methods of calculating the score work as intented and produce the same output.
+					'''
+					final_score = float(score_integrated(main_array, visited_connections, stations, route, critical_stations,(m+1)))
 																
-					temp = highest_visited.copy()
-					route = highest_route.copy()
-				
-				# if the best scoring combination of routes from a certain set of starting stations gets a new highest score, save it as the current all time highest for following iterations
-				if highest_score > all_time_highest_score:
-					all_time_highest_score = highest_score
-					for a in range(trains):
-						all_time_highest_route[a] = highest_route[a].copy()
-					all_time_highest_visited = highest_visited.copy()
-				
-				# after every set of starting stations, write the current all time highest score to a csv file to plot the curve.
-				writer.writerow([round(all_time_highest_score, 5)])	
-		
-		# if the algorithm is interrupted before all iterations are done, signify it and proceed to print current all time best scoring set of routes
-		except KeyboardInterrupt:
-			print("INTERRUPTED")
+					# if a combination of routes gets a new highest score, set it as the current best scoring combination for following iterations
+					if final_score > highest_score:
+						highest_score = final_score
+						highest_route[m] = route[m].copy()
+						highest_visited = visited_connections.copy()
+															
+				temp = highest_visited.copy()
+				route = highest_route.copy()
+			
+			# write calculated highscore of current set of starting stations to csv
+			with open("./csv/breadth_first_all_scores.csv", 'a', newline='') as f:
+				writer = csv.writer(f)
+				writer.writerow([round(final_score, 5)])	
+			
+			# if the best scoring combination of routes from a certain set of starting stations gets a new highest score, save it as the current all time highest for following iterations
+			if highest_score > all_time_highest_score:
+				all_time_highest_score = highest_score
+				for a in range(trains):
+					all_time_highest_route[a] = highest_route[a].copy()
+				all_time_highest_visited = highest_visited.copy()
+			
+			# after every set of starting stations, write the current all time highest score to a csv file to plot the curve.
+			with open("./csv/breadth_first_highscores.csv", 'a', newline='') as f:
+				writer = csv.writer(f)
+				writer.writerow([round(all_time_highest_score, 5)])
+	
+	# if the algorithm is interrupted before all iterations are done, signify it and proceed to print current all time best scoring set of routes
+	except KeyboardInterrupt:
+		print("INTERRUPTED")
 					
 	return all_time_highest_route, all_time_highest_score
